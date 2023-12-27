eeffe8a87e979a7378c103b5238e353a56d3c65c
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2023-12-27 12:56:15 +0430

    payload added

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,19 @@
 import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-        };
-    }
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-        };
-    }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 
-    return state;
+  return state;
 };
 
 const store = createStore(counterReducer);
